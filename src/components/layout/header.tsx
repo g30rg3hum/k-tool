d5f7892d5ce97d4851c543365ebea2fa576ca8a0
Weng Wong (George) Hum <124537582+wengwongg@users.noreply.github.com>
2025-07-17 07:53:25 +0100

    cleanup of header and overall formatting

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import navLinks from "@/lib/consts/nav";
+import { layoutPadding } from "@/lib/consts/padding";
 import { Bars3Icon } from "@heroicons/react/16/solid";
 import clsx from "clsx";
 import Link from "next/link";
@@ -8,74 +9,83 @@ import { useEffect, useState } from "react";
 
 export default function Header() {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-  const [isSufficientlyScrolled, setIsSufficientlyScrolled] = useState(false);
+  // const [isSufficientlyScrolled, setIsSufficientlyScrolled] = useState(false);
 
   const toggleMobileMenu = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
   };
 
   // add event listener for detecting the user has sufficiently scrolled
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollThreshold = 10;
-      setIsSufficientlyScrolled(window.scrollY > scrollThreshold);
-    };
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     const scrollThreshold = 10;
+  //     setIsSufficientlyScrolled(window.scrollY > scrollThreshold);
+  //   };
 
-    window.addEventListener("scroll", handleScroll);
+  //   window.addEventListener("scroll", handleScroll);
 
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
+  //   return () => window.removeEventListener("scroll", handleScroll);
+  // }, []);
+
+  const sizeChangeMenu = "lg";
 
   return (
+    // need to re-specify max-width because fixed.
     <header
-      className={clsx(
-        "fixed top-0 z-10 w-full transition-colors duration-300",
-        isSufficientlyScrolled || isMobileMenuOpen
-          ? "bg-white"
-          : "bg-transparent",
-        !isMobileMenuOpen && isSufficientlyScrolled && "shadow-xs"
-      )}
+      className={`fixed top-5 left-0 right-0 mx-auto z-10 w-full max-w-[975px] ${layoutPadding}`}
     >
-      <div className="flex justify-between items-center px-8 py-6">
-        <h1 className="font-black text-3xl">K-TOOL</h1>
+      {/* Main part of the header */}
+      <div
+        className={clsx(
+          "flex justify-between items-center p-6 rounded-lg transition-colors duration-300 bg-zinc-50 shadow-sm"
+        )}
+      >
+        <h1>K-TOOL</h1>
 
-        {/* Desktop Navigation */}
-        <nav className="hidden lg:block h-max">
-          <ul className="uppercase font-semibold flex gap-6">
+        {/* Desktop full navigation */}
+        <nav className={`hidden ${sizeChangeMenu}:block`}>
+          <ul className="flex gap-6 font-semibold">
             {navLinks.map(({ text, href }) => (
               <NavLink key={text} href={href} text={text} />
             ))}
           </ul>
         </nav>
 
-        {/* Hamburger Button */}
+        {/* Smaller screen: hamburger trigger */}
         <button
-          className="lg:hidden hover:cursor-pointer"
+          className={`${sizeChangeMenu}:hidden hover:cursor-pointer hover:scale-[1.03] transition-all`}
           onClick={toggleMobileMenu}
-          aria-label="Toggle mobile menu"
         >
-          <Bars3Icon className="size-7" />
+          <Bars3Icon className="size-6" />
         </button>
       </div>
 
-      {/* Mobile Menu - overlaps below the header. */}
+      {/* Hamburger menu below the header */}
+      {/* Container to create required padding + width */}
       <div
-        className={`lg:hidden absolute top-full left-0 w-full bg-white shadow-xs transition-all duration-300 ease-in-out ${
-          isMobileMenuOpen ? "opacity-100" : "opacity-0"
-        }`}
+        className={`${sizeChangeMenu}:hidden absolute left-0 top-full mt-5 w-full max-w-[975px] ${layoutPadding}`}
       >
-        <nav className="px-8 pb-6">
-          <ul className="uppercase font-medium space-y-4">
-            {navLinks.map(({ text, href }) => (
-              <NavLink
-                key={text}
-                href={href}
-                text={text}
-                onClick={() => setIsMobileMenuOpen(false)}
-              />
-            ))}
-          </ul>
-        </nav>
+        <div
+          className={clsx(
+            `bg-zinc-50 shadow-sm transition-all duration-300 ease-in-out p-6 rounded-lg`,
+            isMobileMenuOpen
+              ? "opacity-100 translate-0"
+              : "opacity-0 -translate-y-2"
+          )}
+        >
+          <nav>
+            <ul className="font-semibold space-y-3">
+              {navLinks.map(({ text, href }) => (
+                <NavLink
+                  key={text}
+                  href={href}
+                  text={text}
+                  onClick={() => setIsMobileMenuOpen(false)}
+                />
+              ))}
+            </ul>
+          </nav>
+        </div>
       </div>
     </header>
   );
---
+++ src/lib/consts/padding.tsx
@@ -0,0 +1,1 @@
+export const layoutPadding = "px-6 md:px-8 lg:px-10";
